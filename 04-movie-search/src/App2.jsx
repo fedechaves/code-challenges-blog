1bc1642200cc1b087ade32799a3bc9f9cbd4e4b5
fedechaves <federicochavesf@gmail.com>
2023-01-06 12:00:00 +0100

    added new movie header

---
+++ 04-movie-search/src/App2.jsx
@@ -0,0 +1,13 @@
+const App2 = () => {
+  return (
+    <div>
+      <header>
+        <form action="">
+            <input type="text" />
+        </form>
+      </header>
+    </div>
+  )
+}
+
+export default App2
