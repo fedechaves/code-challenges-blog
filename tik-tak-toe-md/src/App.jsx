b3dc35c4840d0a2c79e6c0fb2c863e05952ab11b
fedechaves <federicochavesf@gmail.com>
2023-08-28 18:31:43 -0400

    winner component

--- tik-tak-toe-md/src/App.jsx
+++ tik-tak-toe-md/src/App.jsx
@@ -1,50 +1,14 @@
 import { useState } from 'react';
+import confetti from 'canvas-confetti'
+import Square from './components/Square';
+import { TURNS } from './constants';
+import { checkWinner } from './logic/board';
 import './App.css'
-
-const TURNS = {
-  X: "x",
-  O: "o"
-};
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className=`square ${isSelected ? 'is-selected' : "" }`
-  const handleClick = () => {
-    updateBoard(index)
-  }
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}; 
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
   const [winner, setWinner] = useState(null);
-
-  const checkWinner = (boardToCheck) => {
-    for(const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo;
-      if(boardToCheck[a] &&
-         boardToCheck[a] === boardToCheck[b] &&
-         boardToCheck[b] === boardToCheck[c]){
-          return boardToCheck[a]
-         }
-    }
-    return null
-  }
 
   const updateBoard = (index) => {
     if(board[index] || winner) return
@@ -59,6 +23,7 @@ function App() {
     //look for a winner
     const newWinner = checkWinner(newBoard);
     if(newWinner) {
+      confetti()
       setWinner(newWinner)
     }else if(checkEndGame(newBoard)) {
       setWinner(false)
---
+++ tik-tak-toe-md/src/components/Winner.jsx
@@ -0,0 +1,9 @@
+const Winner = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Winner
