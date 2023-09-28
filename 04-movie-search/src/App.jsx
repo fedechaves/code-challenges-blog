dd55fc829f4b8bad4ee9d16031d15c23d26eae71
fedechaves <federicochavesf@gmail.com>
2023-09-28 11:38:03 -0400

    mock content loaded

--- 04-movie-search/src/App.jsx
+++ 04-movie-search/src/App.jsx
@@ -3,7 +3,8 @@ import responseMovies from './mocks/with-results.json'
 import withoutResults from './mocks/no-results.json'
 
 function App() {
-
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
   return (
     <div className='page'>
       <header>
@@ -14,7 +15,20 @@ function App() {
         </form>
       </header>
       <main>
-        Aquí los resultados...
+        {hasMovies ?(
+          <ul>
+            {movies.map(movie => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            ))}
+          </ul>
+        ): (
+          // eslint-disable-next-line react/no-unescaped-entities
+          <p>Haven't find any movies for this search</p>
+        )}
       </main>
     </div>
   )
