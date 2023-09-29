304c7d041c6180221f3348aea93cccfc65672121
fedechaves <federicochavesf@gmail.com>
2023-09-29 17:28:28 -0400

    added: custom hook for movies

---
+++ 04-movie-search/src/components/Movies.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+export function ListMovies  ({  movies }) {
+  return (
+    <ul>
+      {movies.map(movie => (
+        <li key={movie.id}>
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+          <img src={movie.poster} alt={movie.Title} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export function NoResults() {
+  return (
+    // eslint-disable-next-line react/no-unescaped-entities
+    <p>Haven't find any movies for this search</p>
+  )
+}
+
+export function Movies ({ movies }) {
+  const hasMovies = movies?.length > 0
+
+  return (
+    hasMovies 
+    ? <ListMovies movies={movies} /> 
+    : <NoResults />
+  )
+}
