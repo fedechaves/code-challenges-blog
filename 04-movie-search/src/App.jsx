6753443d31d69f040742d40e89a1eb6dffdf0def
fedechaves <federicochavesf@gmail.com>
2023-10-01 20:07:59 -0300

    delete useRef

--- 04-movie-search/src/App.jsx
+++ 04-movie-search/src/App.jsx
@@ -1,24 +1,24 @@
 import './App.css'
-import { useRef } from 'react'
+//import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const { movies } = useMovies()
-  const inputRef = useRef()
+  //const inputRef = useRef()
   
   const handleSubmit = (event) => {
     event.preventDefault()
-    const { query } = Object.fromEntries(
+    const {query } = Object.fromEntries(
       new window.FormData(event.target))
     console.log(query)
   }
 
   return (
-    <div className='page' onSubmit={handleSubmit}>
+    <div className='page'>
       <header>
       <h1>Movie Finder</h1>
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
           <input name='query' placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Search</button>
         </form>
