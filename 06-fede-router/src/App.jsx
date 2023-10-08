a178febd3a612995dfea17886c266f27a9f0f954
fedechaves <federicochavesf@gmail.com>
2023-10-08 13:00:00 +0100

    added navigation event + pushstate

--- 06-fede-router/src/App.jsx
+++ 06-fede-router/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+  window.history.pushState({}, '',href)
+  const navigationEvent = new Event('pushstate')
+  window.dispatchEvent(navigationEvent)
+}
+
 function HomePage () {
   return (
     <>
@@ -23,6 +31,7 @@ function AboutPage() {
     </>
   )
 }
+
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
@@ -30,7 +39,8 @@ function App() {
     <main>
       {currentPath === '/' && 
       <HomePage />}
-      {currentPath === '/about' && <AboutPage />}
+      {currentPath === '/about' && 
+      <AboutPage />}
     </main>
   )
 }
