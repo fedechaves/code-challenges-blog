17c84c4cfaa159d55a3a662a5444b56225651123
fedechaves <federicochavesf@gmail.com>
2023-10-15 12:43:20 -0300

    todo todos component completed

--- 07-todolist-TS/src/App.tsx
+++ 07-todolist-TS/src/App.tsx
@@ -1,10 +1,44 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
 import './App.css'
 
-const App = (): JSX.Element => {
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false
+  },
+  {
+    id: '4',
+    title: 'todo 4',
+    completed: false
+  }
+]
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = (id: string): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
   return (
-    <>
-     <h1>To do App</h1>
-    </>
+    <div className="todoapp">
+    <Todos
+      onRemoveTodo={handleRemove}
+      todos={todos}/>
+    </div>
   )
 }
 
