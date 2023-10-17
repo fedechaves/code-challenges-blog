5922c7d16b492d5168ebbb1904dfd5e4edf6a116
fedechaves <federicochavesf@gmail.com>
2023-10-17 14:49:41 -0300

    check and unchecked todo funct

--- 07-todolist-TS/src/App.tsx
+++ 07-todolist-TS/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Todos } from './components/Todos'
 import './App.css'
+import { type TodoId, type Todo as TodoType } from './types'
 
 const mockTodos = [
   {
@@ -28,8 +29,22 @@ const mockTodos = [
 const App: React.FC = () => {
   const [todos, setTodos] = useState(mockTodos)
 
-  const handleRemove = (id: string): void => {
+  const handleRemove = ({ id }: TodoId): void => {
     const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = ({ id, completed }: Pick<TodoType, 'id' |
+  'completed'>): void => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+      return todo
+    })
     setTodos(newTodos)
   }
 
@@ -37,7 +52,9 @@ const App: React.FC = () => {
     <div className="todoapp">
     <Todos
       onRemoveTodo={handleRemove}
-      todos={todos}/>
+      todos={todos}
+      onToggleCompleteTodo={handleCompleted}
+    />
     </div>
   )
 }
--- 07-todolist-TS/src/components/Todo.tsx
+++ 07-todolist-TS/src/components/Todo.tsx
@@ -1,22 +1,30 @@
-import { type Todo as TodoType } from '../types'
+import { type TodoId, type Todo as TodoType } from '../types'
 
 interface Props extends TodoType {
-  onRemoveTodo: (id: string) => void
+  onToggleCompleteTodo: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
+  onRemoveTodo: ({ id }: TodoId) => void
 }
 
-export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo }) => {
+export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompleteTodo }) => {
+  const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    onToggleCompleteTodo({
+      id,
+      completed: event.target.checked
+    })
+  }
+
   return (
     <div className='view'>
         <input
             className='toggle'
             checked={completed}
             type='checkbox'
-            onChange={() => {}}
+            onChange={handleChangeCheckbox}
         />
         <label>{title}</label>
         <button
             className='destroy'
-            onClick={() => { onRemoveTodo(id) }}
+            onClick={() => { onRemoveTodo({ id }) }}
         />
     </div>
   )
