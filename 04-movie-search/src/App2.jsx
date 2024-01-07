85e753ed1df6f2af626d70fae587c489bd2aa7be
fedechaves <federicochavesf@gmail.com>
2024-01-07 20:49:12 -0300

    movies displayed

--- 04-movie-search/src/App2.jsx
+++ 04-movie-search/src/App2.jsx
@@ -1,11 +1,30 @@
+import { Search } from './mocks/with-results.json'
+import './App.css'
+import { useState } from 'react'
+
 const App2 = () => {
+    const [isSearch, setSearch] = useState(false)
   return (
-    <div>
+    <div className="page">
       <header>
-        <form action="">
+        <h1>This is Movie App</h1>
+        <form >
             <input type="text" />
+            <button type="submit">Search</button>
         </form>
       </header>
+
+      <main>
+        <ul className="movies">
+            {isSearch ? Search.map((movie, idx) => (
+                <li key={idx} className="movie">
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt="" />
+                </li>
+            )): (<h1>not found</h1>)}
+        </ul>
+      </main>
     </div>
   )
 }
