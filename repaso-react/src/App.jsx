87e7a7dc38f93d6b9bacc43a07d91f1e4f815ad6
fedechaves <federicochavesf@gmail.com>
2024-01-19 12:56:07 -0300

    twitter followcard

--- repaso-react/src/App.jsx
+++ repaso-react/src/App.jsx
@@ -1,16 +1,37 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
 import './App.css'
 
-function TwitterBox({ user, img, username, isFollowing }) {
+
+function TwitterBox({ user, img, username, initialIsFollowing }) {
+  const [ isFollowing, setIsFollowing ] = useState(initialIsFollowing)
+
+  const isText = isFollowing ? 'Siguiendo' : 'Seguir'
+  const followingClass = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+  function handleClick() {
+    setIsFollowing(!isFollowing)
+  }
+
   return(
     <article className='tw-followCard'>
-      <div className='tw-followCard-header'>
-        <img className='tw-followCard-avatar' height={150} src="https://pbs.twimg.com/profile_images/1706686915167064064/Fh5d_3Mk_400x400.jpg" alt="User foto Profile" />
-      </div>
-      <div className='tw-followCard-info'>
-        <strong>{user}</strong>
-        <span className='tw-followCard-infoUserName'>{username}</span>
-      </div>
-      <button className='tw-followCard-button'>Seguir</button>
+      <header className='tw-followCard-header'>
+        <img className='tw-followCard-avatar' 
+              src={img} 
+              alt="User foto Profile" 
+        />
+        <div className='tw-followCard-info'>
+          <strong>{user}</strong>
+          <span className='tw-followCard-infoUserName'>{username}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button onClick={handleClick} className={followingClass}>
+          <span className='tw-followCard-text'>{isText}</span>
+          <span className='tw-followCard-stopFollow'>Dejar de Seguir</span>
+        </button>
+      </aside>
     </article>
   )
 }
@@ -25,14 +46,20 @@ function App() {
       <TwitterBox 
         user='Fede Chaves'
         username='@fedechaves'
+        initialIsFollowing={true}
+        img={"https://pbs.twimg.com/profile_images/1706686915167064064/Fh5d_3Mk_400x400.jpg"}
       />
       <TwitterBox 
         user='Fede Chaves'
         username='@fedechaves'
+        initialIsFollowing={false}
+        img={"https://pbs.twimg.com/profile_images/1706686915167064064/Fh5d_3Mk_400x400.jpg"}
       />
       <TwitterBox 
         user='Fede Chaves'
         username='@fedechaves'
+        initialIsFollowing={false}
+        img={"https://pbs.twimg.com/profile_images/1706686915167064064/Fh5d_3Mk_400x400.jpg"}
       />
     </section>
   )
